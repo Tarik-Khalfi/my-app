63af7e024a9c2e927a71f5972b04993fc1022bc1
Tarik-Khalfi <khtarek07@gmail.com>
2023-10-10 13:05:57 +0100

    age-calculator

--- app/page.js
+++ app/page.js
@@ -1,113 +1,99 @@
-import Image from 'next/image'
-
+"use client";
+import Image from "next/image";
+import { useState } from "react";
 export default function Home() {
+  const [day, setDay] = useState(0);
+  const [month, setMonth] = useState(0);
+  const [year, setYear] = useState(0);
+
+  const [finalDate, setFinalDate] = useState({
+    days: 0,
+    months: 0,
+    years: 0,
+  });
+  function calc(e) {
+    e.preventDefault();
+    let yearslived = new Date().getFullYear() - year;
+    let daysLived = yearslived * 356;
+    let monthsLived = yearslived * 12;
+    setFinalDate({ days: daysLived, months: monthsLived, years: yearslived });
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className=" h-[100vh] flex justify-center items-center mx-auto px-10 bg-[##716F6F]">
+      <div classname="bg-[#FFF] h-fit w-fit  shadow-2xl rounded-br-3xl ">
+        <div className=" flex flex-col space-y-4 ">
+          <div className=" flex flex-row justify-between">
+            <p>day</p>
+            <p>month</p>
+            <p>Year</p>
+          </div>
+          <div className=" flex flex-row border-b-sky-200 border-b-2">
+            <input
+              type="number"
+              placeholder="DD"
+              onChange={(e) => {
+                setDay(e.target.value);
+              }}
+              className="border border-[#DCDCDC] px-4 py-2"
             />
-          </a>
+            <input
+              type="number"
+              placeholder="MM"
+              onChange={(e) => {
+                setMonth(e.target.value);
+              }}
+              className="border border-[#DCDCDC] px-4 py-2"
+            />
+            <input
+              type="number"
+              placeholder="YYYY"
+              onChange={(e) => {
+                setYear(e.target.value);
+              }}
+              className="border border-[#DCDCDC] px-4 py-2"
+            />
+          </div>
+          <div className=" flex flex-row container ">
+            <div className="w-full relative h-[1px]  flex justify-center items-center">
+              <Image
+                src="/Rectangle 6.png"
+                fill
+                alt="line"
+                classname="w-[100%] h-[1px] absolute"
+              />
+            </div>
+            <button
+              onClick={(e) => {
+                calc(e);
+              }}
+            >
+              <Image src="/Frame 30.png" height={96} width={96} alt="button" />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <p className="text-[#151515] font-[880] text-[104px]">
+            <span className="text-[#854DFF] ">
+              {" "}
+              {finalDate.years === 0 ? "--" : finalDate.years}
+            </span>
+            Years
+          </p>
+          <p className="text-[#151515] font-[880] text-[104px]">
+            <span className="text-[#854DFF] ">
+              {finalDate.months === 0 ? "--" : finalDate.months}
+            </span>
+            Month
+          </p>
+          <p className="text-[#151515] font-[880] text-[104px]">
+            <span className="text-[#854DFF] ">
+              {finalDate.days === 0 ? "--" : finalDate.days}
+            </span>
+            days
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
